78c7e9b1d99c168655a01e4a1c1e3ec77f8cdd81
KaanSimsek19 <kaan.simsek@valensas.com>
2023-10-29 14:22:11 +0300

    Add cors config

---
+++ src/main/kotlin/com/sopp/auth/config/CorsConfig.kt
@@ -0,0 +1,22 @@
+package com.sopp.auth.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowedHeaders = listOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
--- src/main/kotlin/com/sopp/auth/controller/LoginController.kt
+++ src/main/kotlin/com/sopp/auth/controller/LoginController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin(origins = ["http://localhost:3000"])
 class LoginController {
 
     @PostMapping
